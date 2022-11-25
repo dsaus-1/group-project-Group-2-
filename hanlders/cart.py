3af7137dcd120f4c1b5b949501b686081fada8d3
AytaDzhivanov1996 <118442161+AytaDzhivanov1996@users.noreply.github.com>
2022-11-25 15:02:06 +0300

    'cart funcs updated'

--- hanlders/cart.py
+++ hanlders/cart.py
@@ -3,21 +3,19 @@ import json
 def put_product_to_cart(data):
     with open('data/catalog.json', encoding='utf-8') as json_file:
         catalog_list = json.load(json_file)
-        data = []
-        id_input = input("id: ")
-        count_input = input("count: ")
+        cart_list = []
         for i in catalog_list:
             for elem in i['products']:
-                if id_input == elem['id']:
-                    data.append({'name': elem['name'], 'id': elem['id'], 'count': count_input})
-                    print(f"'code': 201\n 'message': Товар {data['name']} в количестве {data['count']} штук добавлен в корзину успешно")
-                if id_input != elem['id']:
+                if data["data"]["id"] == elem['id']:
+                    cart_list.append({'name': elem['name'], 'id': elem['id'], 'count': count_input})
+                    print(f"'code': 201\n 'message': Товар {cart_list['name']} в количестве {cart_list['count']} штук добавлен в корзину успешно")
+                if data["data"]["id"] != elem['id']:
                     print("'code': 404,\n 'message': Товара с таким номером не найдено.")
-                if count_input > elem["balance"]:
-                    print(f"Невозможно добавить товар {data['name']} в количестве {count_input} штук в корзину, потому что их осталось всего {elem['balance']}.")
+                if data["data"]["count"] > elem["balance"]:
+                    print(f"Невозможно добавить товар {cart_list['name']} в количестве {count_input} штук в корзину, потому что их осталось всего {elem['balance']}.")
 
     with open('data/cart.json', 'w') as f:
-        json.dump(data, f)
+        json.dump(cart_list, f)
 
 
 def get_cart(data):
