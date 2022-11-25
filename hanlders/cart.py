9338dd111be38a630bd11d9fbe645d6c11028dab
Dima Saushkin <saushkin.reg@gmail.com>
2022-11-25 13:02:02 +0300

    'Всё'

--- hanlders/cart.py
+++ hanlders/cart.py
@@ -1,6 +1,16 @@
+import json
+
 def put_product_to_cart(data):
     pass
 
 
 def get_cart(data):
-    pass
+   with open('data_sample/cart.json', encoding='utf-8') as read_file:
+       read_cart = json.load(read_file)
+       count = 0
+       for i in read_cart:
+           count += 1
+           print(f'{count}. {i['name']} ')
+
+
+get_cart(6)
---
+++ tests/test_get_cart.py
@@ -0,0 +1,8 @@
+from hanlders.cart import get_cart, put_product_to_cart
+
+
+
+
+def test_get_cart():
+    put_product_to_cart(5) =
+    assert get_cart() ==
