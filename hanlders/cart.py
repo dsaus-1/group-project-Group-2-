4c8d3d3a2f7d95b2e441561fba856375ed90486b
AytaDzhivanov1996 <118442161+AytaDzhivanov1996@users.noreply.github.com>
2022-11-25 17:21:31 +0300

    'cart funcs update'

--- hanlders/cart.py
+++ hanlders/cart.py
@@ -9,17 +9,17 @@ def put_product_to_cart(data):
         message = "Товара с таким номер не найдено."
         for i in catalog_list:
             for elem in i['products']:
+                if data["data"]["id"] == elem['id'] and data["data"]["count"] > elem["balance"]:
+                    code = 409
+                    message = f"Невозможно добавить товар {elem['name']} в количестве {data['data']['count']} штук в корзину, потому что их осталось всего {elem['balance']}."
+                    return f"'code': {code},\n'message': {message}"
                 if data["data"]["id"] == elem['id']:
                     code = 201
                     message = f"Товар {elem['name']} в количестве {data['data']['count']} штук добавлен в корзину успешно"
                     cart_list['cart'].append({'name': elem['name'], 'id': elem['id'], 'count': data["data"]["count"]})
-                    return f"'code': {code}\n'message': {message}"
-                elif data["data"]["count"] > elem["balance"]:
-                    code = 409
-                    message = f"Невозможно добавить товар {elem['name']} в количестве {data['data']['count']} штук в корзину, потому что их осталось всего {elem['balance']}."
                     return f"'code': {code},\n'message': {message}"
 
-        return {'code': code, 'message': message}
+    return f"'code': {code},\n'message': {message}"
     with open('data/cart.json', 'w') as f:
         json.dump(cart_list, f)
 
