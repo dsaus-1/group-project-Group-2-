38176ac67f797e45b7b7dd634b450135a0816a7c
AytaDzhivanov1996 <118442161+AytaDzhivanov1996@users.noreply.github.com>
2022-11-25 14:40:44 +0300

    'cart functions updated'

--- hanlders/cart.py
+++ hanlders/cart.py
@@ -4,11 +4,12 @@ def put_product_to_cart(data):
     with open('data_sample/catalog.json', encoding='utf-8') as json_file:
         catalog_list = json.load(json_file)
     data = {}
-    data['filter'] = []
+    id_input = input("id: ")
+    count_input = input("count: ")
     for i in catalog_list:
         for elem in i['products']:
             if id_input == elem['id']:
-                data['filter'].append({'name': elem['name'], 'id': elem['id'], 'count': count_input})
+                data.append({'name': elem['name'], 'id': elem['id'], 'count': count_input})
                 print(f"'code': 201\n 'message': Товар {data['name']} в количестве {data['count']} штук добавлен в корзину успешно")
             if id_input != elem['id']:
                 print("'code': 404,\n 'message': Товара с таким номером не найдено.")
